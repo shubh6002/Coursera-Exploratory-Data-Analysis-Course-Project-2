1ea93377547e55a2255869f41573cd6c3f3d2e85
shubham <66512088+shubh6002@users.noreply.github.com>
2020-06-28 19:50:27 +0530

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -4,23 +4,23 @@ source("downloadArchive.R")
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
-# Subset coal combustion related NEI data
-combustionRelated <- grepl("comb", SCC$SCC.Level.One, ignore.case=TRUE)
-coalRelated <- grepl("coal", SCC$SCC.Level.Four, ignore.case=TRUE) 
-coalCombustion <- (combustionRelated & coalRelated)
-combustionSCC <- SCC[coalCombustion,]$SCC
-combustionNEI <- NEI[NEI$SCC %in% combustionSCC,]
-
-png("plot4.png",width=480,height=480,units="px",bg="transparent")
-
 library(ggplot2)
 
-ggp <- ggplot(combustionNEI,aes(factor(year),Emissions/10^5)) +
-  geom_bar(stat="identity",fill="grey",width=0.75) +
-  theme_bw() +  guides(fill=FALSE) +
-  labs(x="year", y=expression("Total PM"[2.5]*" Emission (10^5 Tons)")) + 
-  labs(title=expression("PM"[2.5]*" Coal Combustion Source Emissions Across US from 1999-2008"))
+# Across the United States, how have emissions from coal combustion-related sources changed from 1999-2008?
 
-print(ggp)
+# fetch all NEIxSCC records with Short.Name (SCC) Coal
+coalMatches  <- grepl("coal", NEISCC$Short.Name, ignore.case=TRUE)
+subsetNEISCC <- NEISCC[coalMatches, ]
 
+aggregatedTotalByYear <- aggregate(Emissions ~ year, subsetNEISCC, sum)
+
+
+
+png("plot4.png", width=640, height=480)
+g <- ggplot(aggregatedTotalByYear, aes(factor(year), Emissions))
+g <- g + geom_bar(stat="identity") +
+  xlab("year") +
+  ylab(expression('Total PM'[2.5]*" Emissions")) +
+  ggtitle('Total Emissions from coal sources from 1999 to 2008')
+print(g)
 dev.off()
