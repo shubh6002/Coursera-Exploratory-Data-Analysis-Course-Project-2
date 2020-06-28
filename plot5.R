0b4fbca1349fbc77a3a81d5fa0062723b34ba1a9
shubham <66512088+shubh6002@users.noreply.github.com>
2020-06-28 19:56:20 +0530

    Update plot5.R

--- plot5.R
+++ plot5.R
@@ -1,21 +1,21 @@
-# Gather the subset of the NEI data which corresponds to vehicles
-vehicles <- grepl("vehicle", SCC$SCC.Level.Two, ignore.case=TRUE)
-vehiclesSCC <- SCC[vehicles,]$SCC
-vehiclesNEI <- NEI[NEI$SCC %in% vehiclesSCC,]
-
-# Subset the vehicles NEI data to Baltimore's fip
-baltimoreVehiclesNEI <- vehiclesNEI[vehiclesNEI$fips=="24510",]
-
-png("plot5.png",width=480,height=480,units="px",bg="transparent")
-
 library(ggplot2)
 
-ggp <- ggplot(baltimoreVehiclesNEI,aes(factor(year),Emissions)) +
-  geom_bar(stat="identity",fill="grey",width=0.75) +
-  theme_bw() +  guides(fill=FALSE) +
-  labs(x="year", y=expression("Total PM"[2.5]*" Emission (10^5 Tons)")) + 
-  labs(title=expression("PM"[2.5]*" Motor Vehicle Source Emissions in Baltimore from 1999-2008"))
+# How have emissions from motor vehicle sources changed from 1999-2008 in Baltimore City?
 
-print(ggp)
+# 24510 is Baltimore, see plot2.R
+# Searching for ON-ROAD type in NEI
+# Don't actually know it this is the intention, but searching for 'motor' in SCC only gave a subset (non-cars)
+subsetNEI <- NEI[NEI$fips=="24510" & NEI$type=="ON-ROAD",  ]
 
+aggregatedTotalByYear <- aggregate(Emissions ~ year, subsetNEI, sum)
+
+
+
+png("plot5.png", width=840, height=480)
+g <- ggplot(aggregatedTotalByYear, aes(factor(year), Emissions))
+g <- g + geom_bar(stat="identity") +
+  xlab("year") +
+  ylab(expression('Total PM'[2.5]*" Emissions")) +
+  ggtitle('Total Emissions from motor vehicle (type = ON-ROAD) in Baltimore City, Maryland (fips = "24510") from 1999 to 2008')
+print(g)
 dev.off()
